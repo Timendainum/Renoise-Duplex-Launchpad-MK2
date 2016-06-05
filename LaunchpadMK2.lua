d488224c698d1aa52fa27f3d6195d040d7452c70
amoetodi <dannyrobinson@gmx.com>
2016-06-05 20:26:39 +0100

    Updated color codes
    Changed the colorspace to use the full space available, added rainbow mode to LargeGrid.

--- LaunchpadMK2.lua
+++ LaunchpadMK2.lua
@@ -1,3 +1,4 @@
+
 --[[----------------------------------------------------------------------------
 -- Duplex.LaunchpadMK2
 ----------------------------------------------------------------------------]]--
@@ -15,33 +16,25 @@ A device-specific class
 
 class "LaunchpadMK2" (MidiDevice)
 
-LaunchpadMK2_COLOR = 
-{
-  {0x00,0x00,0x00,0}, --BLACK
-  {0x80,0x80,0x80,1}, --GREY
-  {0xFF,0xFF,0xFF,3}, --WHITE
-  {0xF0,0x80,0x80,4}, --ROSE
-  {0xFF,0x00,0x00,5}, --RED
-  {0x8B,0x00,0x00,7}, --DARK_RED
-  {0xFF,0x8C,0x00,9}, --ORANGE
-  {0xF5,0xDE,0xB3,12}, --TAN
-  {0xFF,0xFF,0x00,13}, --YELLOW
-  {0xAD,0xFF,0x2F,17}, --YELLOW GREEN
-  {0x00,0xFF,0x00,21}, --GREEN
-  {0x66,0xFF,0x99,29}, --GREEN SPRING
-  {0x66,0xFF,0xCC,33}, --SPRING
-  {0x66,0xFF,0xFF,37}, --LIGHT LIGHT BLUE
-  {0x00,0xFF,0xFF,41}, --LIGHT BLUE
-  {0x00,0x00,0xFF,45}, --BLUE
-  {0x99,0x33,0xFF,49}, --BLUE PURPLE
-  {0xFF,0x00,0xFF,53}, --PINK PURPLE
-  {0xFF,0x33,0x99,57}, --BRIGHT PINK
+color_reference_12 = {
+  {63,0,0}, --RED
+  {63,31,0}, --ORANGE 
+  {63,63,0}, --YELLOW 
+  {31,63,0}, --CHARTREUSE 
+  {0,63,0}, --GREEN 
+  {0,63,31}, --SPRING 
+  {0,63,63}, --CYAN 
+  {0,31,63}, --AZURE 
+  {0,0,63}, --BLUE 
+  {31,0,63}, --VIOLET 
+  {63,0,63}, --MAGENTA 
+  {63,0,31}, --PURPLE 
 }
 
 function LaunchpadMK2:__init(display_name, message_stream, port_in, port_out)
   TRACE("LaunchpadMK2:__init", display_name, message_stream, port_in, port_out)
 
-  self.colorspace = {1,1,1}
+  self.colorspace = {64,64,64}
   
   MidiDevice.__init(self, display_name, message_stream, port_in, port_out)
 
@@ -65,62 +58,52 @@ end
 
 --- override default Device method
 -- @see Device.output_value
-
 function LaunchpadMK2:output_value(pt,xarg,ui_obj)
   TRACE("LaunchpadMK2:output_value(pt,xarg,ui_obj)",pt,xarg,ui_obj)
   
-    --if xarg.skip_echo then
-        --- parameter only exist in the virtual ui
-    --  return Device.output_value(self,pt,xarg,ui_obj)
-    --else
-  
+  --if xarg.skip_echo then
+    --- parameter only exist in the virtual ui
+  --  return Device.output_value(self,pt,xarg,ui_obj)
+  --else
+
     --print("launchpad output value...",rprint(pt.color))
     
-    --This code uses sysex to output color values to the launchpad
-    --it didn't work very well because the RGB color on the screen
-    --never really lined up with the color the Launchpad displayed
-    --due to LED differences.
     
-    --local sysex_num = nil
-    --local sysex_color = {}
-    --sysex_num = self:extract_midi_cc(xarg.value)
-    --if (sysex_num == nil) then
-    --    sysex_num = self:extract_midi_note(xarg.value)
-    --end
+    local skip_hardware = true
+    local rainbow_mode = true --toggle for fun times :D
+    --rprint(xarg) 
     
-    --for i=1, 3 do
-    --  sysex_color[i] = math.floor((pt.color[i]/255)*63)
-    --end
-    
-    print('num',self:extract_midi_note(xarg.value))
-    print('1',pt.color[1])
-    print('2',pt.color[2])
-    print('3',pt.color[3])
-    --print('sys1',sysex_color[1])
-    --print('sys2',sysex_color[2])
-    --print('sys3',sysex_color[3])
-    
-    local skip_hardware = false  --set to true to use sysex values
-    local output_color = 0
-    
-    local smallest_compare = 765
-    
-    for i=1, table.maxn(LaunchpadMK2_COLOR) do
-      local compare_value = 0
-        for j=1,3 do
-          local temp_compare = 0
-          temp_compare = math.abs(pt.color[j]-LaunchpadMK2_COLOR[i][j])
-          compare_value = temp_compare+compare_value
-        end
-      if compare_value < smallest_compare then
-        smallest_compare = compare_value
-        output_color = LaunchpadMK2_COLOR[i][4]
-      end
+    local sysex_num = nil
+    local sysex_color = {}
+    sysex_num = self:extract_midi_note(xarg.value)
+    if (sysex_num == nil) then
+      sysex_num = self:extract_midi_cc(xarg.value)
     end
     
-    --self:send_sysex_message(0x00,0x20,0x29,0x02,0x18,0x0B,sysex_num,sysex_color[1],sysex_color[2],sysex_color[3])
-    
-    return output_color,skip_hardware
+       
+    local rslt = 0
+          
+    for i=1, 3 do
+      if rainbow_mode and pt.val and (xarg.group_name == "LargeGrid") then
+        
+        
+        local x = xarg.row or 0 
+        local y = xarg.column or 0
+        --print(x, y)
+        local color_code = (-3 + x + y + ((y % 2) * 6)) % 12 + 1 --> magic number dance
+        --print(color_code)
+        sysex_color[i] = math.floor(color_reference_12[color_code][i])
+        
+      else
+        sysex_color[i] = math.floor(pt.color[i]/8)
+      end
+      rslt = rslt * 64 + sysex_color[i]
+    end    
+
+    self:send_sysex_message(0x00, 0x20, 0x29, 0x02, 0x18, 0x0B, sysex_num, sysex_color[1], sysex_color[2], sysex_color[3])
+  
+    return rslt, skip_hardware
+
 end
 
 --------------------------------------------------------------------------------
